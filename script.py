fb4fcd2f523ddc4ac5b63926335aeabbf5c8ac61
DelgadoDoan <doanmgd@gmail.com>
2025-11-07 09:01:23 +0800

    add: additional comments in output log

--- script.py
+++ script.py
@@ -9,12 +9,12 @@ import statsmodels.api as sm
 from datetime import date
 
 # define plot dir
-save_dir = "plots"
+save_dir = 'plots'
 os.makedirs(save_dir, exist_ok=True)
 
 
 # load the dataframe
-df = pd.read_csv("data.csv")
+df = pd.read_csv('data.csv')
 
 # |---------- preprocessing ----------|
 
@@ -44,16 +44,16 @@ df_educ = df[df['Type'] == 'Education'].copy()
 
 # |---------- plot settings ----------|
 # main colors
-color_bg = "#1E1B18"
-color_text = "#FFFFFF"
+color_bg = '#1E1B18'
+color_text = '#FFFFFF'
 
 # color palette
-palette1 = sns.color_palette("Paired", len(df['Type'].unique()))[::-1]
-gradient1 = sns.color_palette("crest", len(df['year_quarter'].unique()))[::-1]
+palette1 = sns.color_palette('Paired', len(df['Type'].unique()))[::-1]
+gradient1 = sns.color_palette('crest', len(df['year_quarter'].unique()))[::-1]
 
 # save color palettes for reference
 # define dir
-colors_dir = "colors"
+colors_dir = 'colors'
 os.makedirs(colors_dir, exist_ok=True)
 
 # palette 1
@@ -61,7 +61,7 @@ plt.figure()
 sns.palplot(palette1)
 plt.gca().set_title('Color Palette 1')
 plt.tight_layout()
-palette_path = os.path.join(colors_dir, "palette1.png")
+palette_path = os.path.join(colors_dir, 'palette1.png')
 plt.savefig(palette_path)
 plt.close()
 
@@ -70,13 +70,13 @@ plt.figure()
 sns.palplot(gradient1)
 plt.gca().set_title('Color Gradient 1')
 plt.tight_layout()
-palette_path = os.path.join(colors_dir, "gradient1.png")
+palette_path = os.path.join(colors_dir, 'gradient1.png')
 plt.savefig(palette_path)
 plt.close()
 
 # general
 mpl.rcParams['figure.dpi'] = 600
-mpl.rcParams["figure.figsize"] = 16,8
+mpl.rcParams['figure.figsize'] = 16,8
 
 # title
 mpl.rcParams['figure.titlesize'] = 24
@@ -84,9 +84,9 @@ mpl.rcParams['axes.titlesize'] = 24
 mpl.rcParams['axes.titleweight'] = 'bold'
 
 # colors
-mpl.rcParams["figure.facecolor"] = color_bg
-mpl.rcParams["axes.facecolor"] = color_bg
-mpl.rcParams["savefig.facecolor"] = color_bg
+mpl.rcParams['figure.facecolor'] = color_bg
+mpl.rcParams['axes.facecolor'] = color_bg
+mpl.rcParams['savefig.facecolor'] = color_bg
 
 # text colors
 mpl.rcParams['text.color'] = color_text
@@ -170,7 +170,7 @@ plt.grid(alpha=0.2)
 plt.tight_layout()
 
 # save to plot dir
-save_path = os.path.join(save_dir, "tsa.png")
+save_path = os.path.join(save_dir, 'tsa.png')
 plt.savefig(save_path)
 plt.close()
 
@@ -185,19 +185,19 @@ plt.figure(figsize=(16, 16))
 plt.subplot(2, 2, 1)
 sns.histplot(df_educ['HFCE'], kde=True, bins=20, color='#ADE8F4')
 plt.title('HFCE in Education', fontsize=16)
-plt.xlabel("HFCE in Education (in million pesos)", fontsize=12)
-plt.ylabel("Count", fontsize=12)
+plt.xlabel('HFCE in Education (in million pesos)', fontsize=12)
+plt.ylabel('Count', fontsize=12)
 plt.tick_params(axis='both', which='major', labelsize=11)
 
 plt.subplot(2, 2, 2)
 sns.histplot(df_educ['GVA'], kde=True, bins=20, color='#ADE8F4')
 plt.title('GVA', fontsize=16)
-plt.xlabel("GVA (in million pesos)", fontsize=12)
-plt.ylabel("Count", fontsize=12)
+plt.xlabel('GVA (in million pesos)', fontsize=12)
+plt.ylabel('Count', fontsize=12)
 plt.tick_params(axis='both', which='major', labelsize=11)
 
 # save to plot dir
-save_path = os.path.join(save_dir, "dist.png")
+save_path = os.path.join(save_dir, 'dist.png')
 plt.savefig(save_path, bbox_inches='tight')
 plt.close()
 
@@ -211,19 +211,19 @@ plt.figure(figsize=(16, 16))
 plt.subplot(2, 2, 1)
 sns.histplot(df_educ['HFCE_log'], kde=True, bins=20, color='#DAFFBA')
 plt.title('HFCE in Education (post-log)', fontsize=16)
-plt.xlabel("HFCE in Education (in million pesos)", fontsize=12)
-plt.ylabel("Count", fontsize=12)
+plt.xlabel('HFCE in Education (in million pesos)', fontsize=12)
+plt.ylabel('Count', fontsize=12)
 plt.tick_params(axis='both', which='major', labelsize=11)
 
 plt.subplot(2, 2, 2)
 sns.histplot(df_educ['GVA_log'], kde=True, bins=20, color='#DAFFBA')
 plt.title('GVA (post-log)', fontsize=16)
-plt.xlabel("GVA (in million pesos)", fontsize=12)
-plt.ylabel("Count", fontsize=12)
+plt.xlabel('GVA (in million pesos)', fontsize=12)
+plt.ylabel('Count', fontsize=12)
 plt.tick_params(axis='both', which='major', labelsize=11)
 
 # save to plot dir
-save_path = os.path.join(save_dir, "postlog_dist.png")
+save_path = os.path.join(save_dir, 'postlog_dist.png')
 plt.savefig(save_path, bbox_inches='tight')
 plt.close()
 
@@ -258,12 +258,16 @@ plt.title('HFCE vs GVA (post-log)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=11)
 
 # save to plot dir
-save_path = os.path.join(save_dir, "scatter.png")
+save_path = os.path.join(save_dir, 'scatter.png')
 plt.savefig(save_path, bbox_inches='tight')
 plt.close()
 
 
 # Pearson's test
+print('|-------------------------------------------------------|')
+print('|----------------- CORRELATION ANALYSIS ----------------|')
+print('|-------------------------------------------------------|')
+
 corr,_ = pearsonr(
     df_educ['HFCE'], 
     df_educ['GVA'], 
@@ -277,6 +281,7 @@ corr_log,_ = pearsonr(
 )
 
 print('Pearson\'s correlation (post-log): %.4f' % corr_log)
+print('')
 
 
 # |-------------------------------------------------------|
@@ -307,6 +312,9 @@ X = df_educ[['time', 'yolanda', 'time_after_yolanda', 'covid', 'time_after_covid
 X = sm.add_constant(X)
 model = sm.OLS(df_educ['HFCE'], X).fit()
 
+print('|-------------------------------------------------------|')
+print('|----------- INTERRUPTED TIME SERIES ANALYSIS ----------|')
+print('|-------------------------------------------------------|')
 print(model.summary())
 
 # predict values
@@ -361,6 +369,6 @@ plt.grid(alpha=0.2)
 plt.tight_layout()
 
 # save to plot dir
-save_path = os.path.join(save_dir, "itsa.png")
+save_path = os.path.join(save_dir, 'itsa.png')
 plt.savefig(save_path)
 plt.close()
